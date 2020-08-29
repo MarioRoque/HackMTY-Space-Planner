f3f95448ad8095e94dc60044adb11d40585e1002
Mario Roque <mm-roque@outlook.com>
2020-08-29 18:32:42 -0500

    Random distribution and map generation

--- PathPlanning/Space-Planner.py
+++ PathPlanning/Space-Planner.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from shapely.geometry import Point
-from shapely.geometry.polygon import Polygon
-
-
 """
 Created on Sat Aug 29 12:41:06 2020
 
@@ -17,13 +13,27 @@ Created on Sat Aug 29 12:41:06 2020
 """
     Inputs: 
         -Maximo de personas
+        -Numero de mesas
         -Threshold de personas
         -Cuatro puntos para el tamaño del cuarto.
         
     Outputs: 
         - JSON, con Array de coordenadas
-
 """
+
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+#Global variables
+usableTables = 0
+numberTables = 0
+percentajeOfUse = 0
+minDistance = 0
+debugFlag = True
 
 polygons = {
     "box1" : [ [0,0],[15,0],[15,15],[0,15]],
@@ -31,21 +41,93 @@ polygons = {
     "church" : [[1,0],[1,2],[0,2],[0,3],[1,3],[1,4],[2,4],[2,3],[3,3],[3,2],[2,2],[2,0]]
 }
 
+walkingPath = {
+    "test": []
+    }
+
+
+#layout[0] = Room Corners, #layout[1] = Path para caminar  #layout[2] = Table position
+layout = [[0,0],[0,0],[0,0]]
+
+
+def debug_walkingPath():
+    #This function generates a walking path separated on columns by 3 meters
+    
+    global walkingPath
+    
+    for i in range(0,15):
+        y = i+1.5
+        if(isInside([1,y], polygons["box1"])):
+            walkingPath['test'].append([1,y])
+
+         
+    for i in range(0,15):
+        y = i+1.5
+        if(isInside([1,y], polygons["box1"])):
+            walkingPath['test'].append([4,y])
+
+
+    for i in range(0,15):
+        y = i+1.5
+        if(isInside([1,y], polygons["box1"])):
+            walkingPath['test'].append([7,y])
+
+    for i in range(0,15):
+            y = i+1.5
+            if(isInside([1,y], polygons["box1"])):
+                walkingPath['test'].append([10,y])
+
+
+    for i in range(0,15):
+            y = i+1.5
+            if(isInside([1,y], polygons["box1"])):
+                walkingPath['test'].append([13,y])
+    return 0
 
 
 def readInputs():
+    global MaxPeople
+    global numberTables
+    global percentajeOfUse
+    global minDistance 
+    #roomCorners = input()
+    
+    MaxPeople = input()
+    numberTables = input()
+    percentajeOfUse = input()
+    minDistance = input()
     
     return 0
 
-def createRoom():
-    
-    
-    return 0
 
-
-def distributeRandom():
     
-    return 0
+def createRoom(polygon, walkingPath,tables):
+    '''
+       Inputs: polygon e.g [[0,0],[1,1],...] and a walking path walkingPath e.g [[0,0],[1,1],...]
+    '''
+    rx,ry = zip(*polygon)
+    px,py = zip(*walkingPath)
+    tx,ty = zip(*tables)
+    plt.scatter(rx,ry, color = 'k')
+    plt.scatter(px,py, color = 'g')
+    plt.scatter(tx,ty, color = 'b')
+
+    return [[rx,ry],[px,py],tables]
+
+def distributeRandom(polygon,usableTables):
+    '''
+    this function distributes the tables on random places, it needs to verify the distances
+    '''
+    global numberTables
+    tables = []
+    point = np.random.random_sample((1, 2))*10
+    for i in range(0, numberTables):
+        point = np.random.random_sample((1, 2))*10
+        while(not isInside([point[0][0],point[0][1]],polygon)):
+            point = np.random.random_sample((1, 2))*10
+        tables.append([point[0][0], point[0][1]])
+        
+    return tables
 
 def excludePoints(object_coordinates,polygon, inside=True):
     '''
@@ -131,6 +213,28 @@ def sendToUnity():
 
     
 def main():
-    
-    
+    global debugFlag
+    if(debugFlag):
+        debug()
+    else:
+        pass
     return 0
+
+def debug():
+    global usableTables
+    global numberTables
+    global percentajeOfUse
+    global minDistance
+    
+    numberTables = 20
+    percentajeOfUse = 50
+    minDistance = 1.5
+    usableTables= numberTables* percentajeOfUse/100
+    debug_walkingPath()
+    tables = distributeRandom(polygons['box1'],usableTables)
+    createRoom(polygons['box1'],walkingPath['test'],tables)
+
+ 
+main()   
+    
+    
