b786330574b1a9e9c72e4156430e4a7ec61d3995
Mario Roque <A01282301@itesm.mx>
2020-08-29 13:45:11 -0500

    First commit: Function constructors

---
+++ PathPlanning/Space-Planner.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 29 12:41:06 2020
+
+@authors: 
+   Ricardo Cazares 
+   Mario Roque 
+   Greg Espinoza
+   Javier Garcia      
+"""
+
+"""
+    Inputs: 
+        -Maximo de personas
+        -Threshold de personas
+        -Cuatro puntos para el tamaño del cuarto.
+        
+    Outputs: 
+        - JSON, con Array de coordenadas
+
+"""
+
+def readInputs():
+    
+    return 0
+
+def createRoom():
+    
+    
+    return 0
+
+
+def distributeRandom():
+    
+    return 0
+
+
+def excludePoints():
+    
+    return 0
+
+
+def selectTables():
+        
+    return 0
+
+
+def verifyDistace():
+    
+    return 0
+
+
+def verifyGlobalDistance():
+    
+    return 0
+
+
+def regeneratePoints():
+    
+    return 0
+
+def warnings():
+
+    return 0
+
+
+def plotPoints():
+    
+    return 0
+
+def createJSON():
+    
+    return 0
+
+def sendToUnity():
+    
+    return 0
+
+    
+def main():
+    
+    
+    return 0
